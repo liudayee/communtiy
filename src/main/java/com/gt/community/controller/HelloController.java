b5dab4a5f576d61b8debe3e901a03f6954931a41
liudayee <ajjrx584520@icloud.com>
2019-09-04 11:21:04 +0800

    hello页面处理

--- src/main/java/com/gt/community/controller/HelloController.java
+++ src/main/java/com/gt/community/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.gt.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-   public String hello(){
-       return "hello";
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name , Model model){
+        model.addAttribute("name",name);
+        return "hello";
    }
 }
